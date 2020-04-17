41b500346c0c5a2271245b4c0207092ba7ecf40e
Malika01 <59484953+Malika01@users.noreply.github.com>
2020-04-17 22:55:00 +0530

    "appear" animation related changes

--- vtcwebsite.js
+++ vtcwebsite.js
@@ -4,5 +4,6 @@ window.onscroll = function() {myFunction()};
                 if(document.body.scrollTop > 1 || document.documentElement.scrollTop > 1)
                 {
                     document.getElementById("text").className="slide";
+                    document.getElementById("text2").className="appear";
                 }
             }
